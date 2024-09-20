98ef2ca731af31acc2c15e81f0946aabe4f9d7db
GabrielaDamascenoRibeiro <gabrieladamascenor@gmail.com>
2024-09-20 10:49:01 -0300

    feat: adds img assistants

--- backend.py
+++ backend.py
@@ -1,9 +1,28 @@
 from openai import OpenAI
+import requests
 
+#Base Functions
 def set_key(api_key):
     return (OpenAI(api_key = api_key))
 
-def set_assistant(client):
+def text_to_audio(text, audio, client):
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=text
+    )
+    response.stream_to_file(audio)
+
+def audio_to_text(audio_file, client): 
+    with open (audio_file, 'rb') as audio_file:
+        response = client.audio.transcriptions.create(
+            model="whisper-1", 
+            file=audio_file
+        )
+        return response.text
+
+#Open Convo Assistants
+def op_set_assistant(client):
     assistant = client.beta.assistants.create(
         name="English Teacher",
         instructions="You are a personal english conversational teacher, your goal is to keep an english conversation going and correcting eventual mistakes. Do your best to always keep a conversation, and only speak in the student's native language if necessary.",
@@ -14,15 +33,7 @@ def set_assistant(client):
     assistant_id = assistant.id
     return (thread_id, assistant_id)
 
-def transcribe_audio_with_whisper(audio_file, client): 
-    with open (audio_file, 'rb') as audio_file:
-        response = client.audio.transcriptions.create(
-            model="whisper-1", 
-            file=audio_file
-        )
-        return response.text
-
-def get_assistant_response(text,client,thread_id, assistant_id):
+def op_get_assistant_response(text,client,thread_id, assistant_id):
     message = client.beta.threads.messages.create(
         thread_id=thread_id,
         role="user",
@@ -31,7 +42,7 @@ def get_assistant_response(text,client,thread_id, assistant_id):
     run = client.beta.threads.runs.create(
         thread_id=thread_id,
         assistant_id=assistant_id,
-        instructions="Please address the user as Gabi. Gabi is a Portuguese native speaker who needs to practice her english conversation skills."
+        instructions= "Please address the user as Gabi. Gabi is a Portuguese native speaker who needs to practice her english conversation skills."
     )
     
     import time
@@ -47,10 +58,77 @@ def get_assistant_response(text,client,thread_id, assistant_id):
     )
     return messages.data[0].content[0].text.value
 
-def text_to_audio(text, audio, client):
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice="alloy",
-        input=text
+#Img Convo Assistants
+def img_get_assistant_response(text, img, client):
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {client}"
+    }
+
+    payload = {
+    "model": "gpt-4o-mini",
+    "messages": [
+        {"role": "system", 
+         "content": "You are a personal english conversational teacher, your goal is to keep an english conversation going and correcting eventual mistakes. Do your best to always keep a conversation, and only speak in the student's native language if necessary. Please address the user as Gabi. Gabi is a Portuguese native speaker who needs to practice her english conversation skills."
+        },
+        {
+        "role": "user",
+        "content": [
+                {
+                "type": "text",
+                "text": text
+                },
+                {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/jpeg;base64,{img}"
+                }
+                }
+            ]
+        }
+    ],
+    "max_tokens": 3000
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    response_data = response.json()
+    return response_data['choices'][0]['message']['content']
+
+
+#Pdf Convo Assistants
+def pdf_set_assistant(client):
+    assistant = client.beta.assistants.create(
+        name="English Teacher",
+        instructions="You are a personal english conversational teacher, your goal is to keep an english conversation going and correcting eventual mistakes. Do your best to always keep a conversation, and only speak in the student's native language if necessary.",
+        model="gpt-4o"
     )
-    response.stream_to_file(audio)
+    thread = client.beta.threads.create()
+    thread_id = thread.id
+    assistant_id = assistant.id
+    return (thread_id, assistant_id)
+
+def pdf_get_assistant_response(text,client,thread_id, assistant_id):
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=text
+    )
+    run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id,
+        instructions= "Please address the user as Gabi. Gabi is a Portuguese native speaker who needs to practice her english conversation skills."
+    )
+    
+    import time
+    while run.status != 'completed':
+        time.sleep(1)
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread_id,
+            run_id=run.id
+        )
+    
+    messages = client.beta.threads.messages.list(
+        thread_id=thread_id
+    )
+    return messages.data[0].content[0].text.value
--- frontend.py
+++ frontend.py
@@ -1,35 +1,115 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 import backend
+from PIL import Image
+import base64
 
-def bot_start():
+#Base Functions
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+#Bot Functions
+def open_convo_bot(client,thread_id, assistant_id):
     recorded_audio = audio_recorder()
     if recorded_audio:
         audio_file = 'audio.mp3'
         with open(audio_file, 'wb') as f:
             f.write(recorded_audio)
         
-        transcribed_text = backend.transcribe_audio_with_whisper(audio_file, st.session_state.client)
+        transcribed_text = backend.audio_to_text(audio_file,client)
         st.write(f"Texto transcrito: {transcribed_text}")
         
-        response_text = backend.get_assistant_response(transcribed_text, st.session_state.client, st.session_state.thread_id, st.session_state.assistant_id)
+        response_text = backend.op_get_assistant_response(transcribed_text,client,thread_id,assistant_id)
         st.write(f"Resposta do chatbot: {response_text}")
         
         response_audio = 'response_audio.mp3'
-        backend.text_to_audio(response_text, response_audio, st.session_state.client)
+        backend.text_to_audio(response_text, response_audio,client)
         st.audio(response_audio)
 
-st.title("English Practice Chatbot")
+def img_convo_bot(client,key):
+    if 'vbase64_image' not in st.session_state:
+        st.session_state.vbase64_image = None
+    if 'uploaded_file' not in st.session_state:
+        st.session_state.uploaded_file = None
+    if 'img_path' not in st.session_state:
+        st.session_state.img_path = None
 
-if 'client' not in st.session_state:
-    api_key = st.text_input("Digite sua chave da API da OpenAI", type="password")
+    if st.session_state.vbase64_image is None:
+        st.session_state.uploaded_file = st.file_uploader("Escolha uma imagem para guiar a conversa!", type=["jpg"])
 
-    if api_key:
-        st.session_state.client = backend.set_key(api_key)
-        st.session_state.thread_id, st.session_state.assistant_id = backend.set_assistant(st.session_state.client)
-        st.success("API Key validada e assistente configurado. Você pode começar a gravar áudios.")
-        bot_start()
-else:
-    bot_start()
+        if st.session_state.uploaded_file:
+            image = Image.open(st.session_state.uploaded_file)
+            st.session_state.img_path = "image.jpg"
+            image.save(st.session_state.img_path)
+            st.session_state.vbase64_image = encode_image(st.session_state.img_path)
+            st.image(st.session_state.img_path, caption="Imagem Selecionada para Conversa", width=200)
+            st.success("Imagem carregada com sucesso! Agora você pode gravar o áudio.")
     
-#key: sk-hTkXQgSv6FHdbQiN318PdNy9QZQfp-_n6-GZc0FoFiT3BlbkFJuyh1urSnrm3CVQzUrSrkSVL4LNCxec1LMR99LzvCUA
+    if st.session_state.vbase64_image is not None:
+        recorded_audio = audio_recorder()
+
+        if recorded_audio:
+            audio_file = 'audio.mp3'
+            with open(audio_file, 'wb') as f:
+                f.write(recorded_audio)
+
+            transcribed_text = backend.audio_to_text(audio_file, client)
+            st.write(f"Texto transcrito: {transcribed_text}")
+
+            response_text = backend.img_get_assistant_response(transcribed_text, st.session_state.vbase64_image, key)
+            st.write(f"Resposta do chatbot: {response_text}")
+
+            response_audio = 'response_audio.mp3'
+            backend.text_to_audio(response_text, response_audio, client)
+            st.audio(response_audio)
+
+def pdf_convo_bot():
+    st.write('pdf convoo')
+    return
+
+#Main App
+def main():
+    if 'bot_selecionado' not in st.session_state:
+        st.session_state.bot_selecionado = None
+    if 'api_key' not in st.session_state:
+        st.session_state.api_key = None
+    if 'client' not in st.session_state:
+        st.session_state.client = None
+
+    if st.session_state.bot_selecionado is None:
+        st.session_state.api_key = st.text_input("Digite sua chave da API da OpenAI", type="password")
+        if st.session_state.api_key:
+            st.session_state.client = backend.set_key(st.session_state.api_key)
+            st.success("API Key validada e assistente configurado. Você pode começar a gravar áudios.")
+
+            opcao = st.radio(
+                "Selecione um Bot:",
+                ('Conversa Aberta', 'Conversa de Imagem', 'Conversa de PDF')
+            )
+            if opcao == 'Conversa Aberta':
+                thread_id, assistant_id = backend.op_set_assistant(st.session_state.client)
+                st.session_state.bot_selecionado = open_convo_bot(st.session_state.client,thread_id, assistant_id)
+            elif opcao == 'Conversa de Imagem':
+                st.session_state.bot_selecionado = img_convo_bot(st.session_state.client, st.session_state.api_key)
+            elif opcao == 'Conversa de PDF':
+                thread_id, assistant_id = backend.pdf_set_assistant(st.session_state.client)
+                st.session_state.bot_selecionado = pdf_convo_bot(st.session_state.client,thread_id, assistant_id)
+
+    else:
+        if st.session_state.bot_selecionado == "Conversa Aberta":
+            open_convo_bot(st.session_state.client,thread_id, assistant_id)
+        elif st.session_state.bot_selecionado == "Conversa de Imagem":
+            img_convo_bot(st.session_state.client,st.session_state.api_key)
+        elif st.session_state.bot_selecionado == "Conversa de PDF":
+            pdf_convo_bot(st.session_state.client,thread_id, assistant_id)
+
+        if st.button("Trocar de Bot"):
+            st.session_state.bot_selecionado = None
+
+st.title("Language Practice Chatbot")
+
+if __name__ == '__main__':
+    main()
+
+#key: sk-LSNmtTh2C7GqteP_5u5U6b69fEZy1dGbSgPlf7KRRBT3BlbkFJyURC53IuQtM53uFC7Qmklhw3-euuYibZ2i-uhvM74A
